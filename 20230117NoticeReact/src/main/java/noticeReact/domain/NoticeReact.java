7527f3231ac1f25fd999de266f2efaa45426c595
potato <ymcho.dev@gmail.com>
2023-01-17 17:32:07 +0900

    JSP & DB connect study

---
+++ 20230117NoticeReact/src/main/java/noticeReact/domain/NoticeReact.java
@@ -0,0 +1,14 @@
+package noticeReact.domain;
+
+import lombok.Data;
+
+@Data
+public class NoticeReact {
+	private int notice_idx;
+	private String title;
+	private String writer;
+	private String content;
+	private String regdate;
+	private int hit;
+	
+}
